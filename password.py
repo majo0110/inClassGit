096534ef03aced0c711b2abb32acff0655158167
Jonathan <majo@oregonstate.edu>
2021-04-16 15:46:12 -0700

    Verify user input to see if it is a number

--- password.py
+++ password.py
@@ -4,6 +4,13 @@ def genPassword(userInput):
 def main():
     userInput = input("Enter a number: ")
 
+    try:
+        userInt = int(userInput)
+        if(userInt == 0):
+            print("You have chosen 0. No input")
+
+    except ValueError:
+        print("You did not enter a valid number. Program will exit")
 
 if __name__ == "__main__":
     main()
