a92baf71d509a3aa7c9c1a7f2d129bbc0ded912c
Jonathan <majo@oregonstate.edu>
2021-04-16 15:26:23 -0700

    verify user input is a number

--- divisors.py
+++ divisors.py
@@ -1,10 +1,16 @@
-def divisors(userNum):
-    print(userNum)
-
-
 def main():
-    userNum = input("Enter a number: ")
-    divisors(userNum)
+    userInput = input("Enter a number: ")
+
+    try:
+        userInt = int(userInput)
+        if(userInt == 0):
+            print("You have chosen 0. No input")
+        divisors(userInt)
+
+    except ValueError:
+        print("You did not enter a valid number. Program will exit")
+
+
 
 if __name__ == "__main__":
     main()
