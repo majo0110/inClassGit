4a89ddbd839908b77e0f7a089f811d57b46ba1e6
Jonathan <majo@oregonstate.edu>
2021-04-16 15:37:08 -0700

    Print message for list of divisor

--- divisors.py
+++ divisors.py
@@ -4,6 +4,9 @@ def divisors(userNum):
         if( (userNum % i) == 0 ):
             divisorList.append(i)
 
+    print("This is a list of divisors for", userNum, divisorList)
+
+
 def main():
     userInput = input("Enter a number: ")
 
